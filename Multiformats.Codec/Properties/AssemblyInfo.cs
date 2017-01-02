fd3e7b2a2a218a37d7fe5a2b0cccb8210ab91be3
Trond Bråthen <tabrath@gmail.com>
2017-01-02 14:42:09 +0100

    added missing fields

--- Multiformats.Codec/Properties/AssemblyInfo.cs
+++ Multiformats.Codec/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Multiformats.Codec")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Multicodec")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("tabrath")]
 [assembly: AssemblyProduct("Multiformats.Codec")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCopyright("Copyright © tabrath 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
